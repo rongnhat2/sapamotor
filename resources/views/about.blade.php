968f74a8465c12b0c9c314b01327fffe3c0b7ab0
Nguyen Nhat Long <long.nguyen2198@gmail.com>
2023-11-15 21:51:28 +0700

    update data

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -16,9 +16,9 @@
                 <div class="about-wrapper">
                     <h3 class="about-title">About us</h3>
                     <p class="about-description"> Established in 2013, SAPA Motor sells electronic equipment, electronic components. We also provides OEM, ODM services for Viet Nam as long as global companies.</p>
-                    <p>We have strong knowledge about designing products in domain of industrial automation, automotive, telecommunication, power electronics.</p>
-                    <p>We have solid partners of many companies about electronics component distributors, mechanical engineering service companies.</p>
-                    <p>Our mission is helping the clients to design and manufacture the best electronic products.</p> 
+                    <p class="about-description">We have strong knowledge about designing products in domain of industrial automation, automotive, telecommunication, power electronics.</p>
+                    <p class="about-description">We have solid partners of many companies about electronics component distributors, mechanical engineering service companies.</p>
+                    <p class="about-description">Our mission is helping the clients to design and manufacture the best electronic products.</p> 
                 </div>
             <img src="{{ asset("assets/images/map.png") }}" alt="">
             </div>
