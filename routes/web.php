3ca8cbd76213671b0da16eab675285ab5e324fdd
Nguyen Nhat Long <long.nguyen2198@gmail.com>
2024-01-02 14:11:47 +0700

    remove page

--- routes/web.php
+++ routes/web.php
@@ -15,7 +15,7 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', 'DisplayController@index')->name('customer.view.index'); 
 Route::get('/about', 'DisplayController@about')->name('customer.view.about'); 
-Route::get('/automotive-sensor', 'DisplayController@automotive')->name('customer.view.automotive'); 
+// Route::get('/automotive-sensor', 'DisplayController@automotive')->name('customer.view.automotive'); 
 Route::get('/charging-solution', 'DisplayController@charging')->name('customer.view.charging'); 
 Route::get('/motor-controllers', 'DisplayController@motor')->name('customer.view.motor'); 
 Route::get('/battery-management', 'DisplayController@battery')->name('customer.view.battery'); 
